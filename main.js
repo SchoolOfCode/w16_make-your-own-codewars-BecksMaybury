d1827fc4160e8682b44240dbd417c0abb7d5b78d
Becks Maybury <becksmaybury@hotmail.co.uk>
2021-07-26 13:09:12 +0100

    start of scenario

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 /* 
 👉 Write your kata here!
+
+Weather Forecaster
+Given a temperature, your weather forecaster should tell you whether it is cold, cool, warm, hot or scorchio.
+The temperature will be a number (can be negative, not always a whole number).
+Temperatures below 5 are cold, between 5 and 12 are cool, between 13 and 18 are warm, between 19 and 25 are hot and anything over 25 are scorchio.
+
+? add a temperature scale eg Celcius of Farenheit
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
